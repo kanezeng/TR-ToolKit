1b6a2217eb17be0ed56b3ab0ed9e057a1def584b
Zhijian Zeng <wintera.zeng@gmail.com>
2012-02-15 12:53:47 +0800

    First checkin. Create a basic function for Native2ASCII

---
+++ src/com/kanezeng/Translation/MainStep.java
@@ -0,0 +1,22 @@
+/**
+ *  Copyright 2012 Zhijian Zeng, All Rights Reserved.
+ *  Email: me@kanezeng.com
+ *	This file is created at 2012-2-15
+ */
+package com.kanezeng.Translation;
+
+/**
+ * This is use by Kane to test the APIs only.
+ * You can simply ignore it.
+ */
+public class MainStep {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Test Native2ASCII.toASCII()
+		System.out.println(Native2ASCII.toASCII("w≈∂ «‘¯÷æº·haha!"));
+	}
+
+}
---
+++ src/com/kanezeng/Translation/Native2ASCII.java
@@ -0,0 +1,45 @@
+/**
+ *  Copyright 2012 Zhijian Zeng, All Rights Reserved.
+ *  Email: me@kanezeng.com
+ *	This file is created at 2012-2-15
+ */
+package com.kanezeng.Translation;
+
+/**
+ * The class provide a method to convert native characters to unicode excaped format like: \u0a31
+ */
+public class Native2ASCII {
+
+	private static String hexString="0123456789ABCDEF";
+	
+	public static String toASCII(String str) {
+		StringBuilder sb = new StringBuilder();
+		
+		try {
+			byte[] bytes = str.getBytes("Unicode");
+			
+			for (int i = 0; i<bytes.length -1 ;i +=2) {
+				if(bytes[i] ==0 && bytes[i+1] <=127) {
+					char[] chars = Character.toChars(bytes[i+1]);
+					sb.append(new String(chars));
+				} else {
+					sb.append("\\u");
+					sb.append(hexString.charAt((bytes[i] & 0xf0) >> 4));
+					sb.append(hexString.charAt((bytes[i] & 0x0f) >> 0));
+					sb.append(hexString.charAt((bytes[i+1] & 0xf0) >> 4));
+					sb.append(hexString.charAt((bytes[i+1] & 0x0f) >> 0));
+				}
+			}
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		String myresult = sb.toString();
+
+		if (myresult.toLowerCase().startsWith("\\ufeff")) {
+			myresult = myresult.substring(6);
+		}
+			
+		return myresult;
+	}
+}
