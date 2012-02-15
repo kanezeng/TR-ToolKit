47f2765bfa08d3ae2770299752d70ab4186c7943
Zhijian Zeng <wintera.zeng@gmail.com>
2012-02-15 15:46:02 +0800

    Add Language section

---
+++ src/com/kanezeng/Translation/Languages/LanguageObject.java
@@ -0,0 +1,18 @@
+/**
+ *  Copyright 2012 Zhijian Zeng, All Rights Reserved.
+ *  Email: me@kanezeng.com
+ *	This file is created at 2012-2-15
+ */
+package com.kanezeng.Translation.Languages;
+
+/**
+ * 
+ */
+public class LanguageObject {
+	public String twoLetterCode = "";
+	public String threeLetterCode = "";
+	public String pseudoPrefix = "";
+	public String pseudoSurfix = "";
+	public String nameEnglish = "";
+	public String nameLocalized = "";
+}
---
+++ src/com/kanezeng/Translation/Languages/SimplifiedChinese.java
@@ -0,0 +1,20 @@
+/**
+ *  Copyright 2012 Zhijian Zeng, All Rights Reserved.
+ *  Email: me@kanezeng.com
+ *	This file is created at 2012-2-15
+ */
+package com.kanezeng.Translation.Languages;
+
+/**
+ * A language object represent Simplified Chinese
+ */
+public class SimplifiedChinese extends LanguageObject {
+	SimplifiedChinese() {
+		this.nameEnglish = "Simplified Chinese";
+		this.nameLocalized = "简体中文";
+		this.twoLetterCode = "CN";
+		this.threeLetterCode = "CHS";
+		this.pseudoPrefix = "开始";
+		this.pseudoSurfix = "结束";
+	}
+}
