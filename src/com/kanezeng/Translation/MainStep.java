0629d34e9d3db6739008a36f04960041add454a1
Kane Zeng <wintera.zeng@gmail.com>
2013-03-04 22:22:56 +0800

    Update build script
    build.xml for ant is changed.

--- src/com/kanezeng/Translation/MainStep.java
+++ src/com/kanezeng/Translation/MainStep.java
@@ -25,7 +25,7 @@ public class MainStep {
 		// Test Native2ASCII.toASCII()
 		// System.out.println(Native2ASCII.toASCII("wŶ����־��haha!"));
 //		 Test LevenshteinDistance
-		 System.out.println(LevenshteinDistance.getDistance("This is Kane",		 "Tha is Kane"));
+		 System.out.println(LevenshteinDistance.getDistance("This is Kane!!",		 "Tha is Kane"));
 		// System.out.println(LevenshteinDistance.getDifferentPercentage("This is Kane",
 		// "Tha is Kane"));
 		// System.out.println(LevenshteinDistance.getSimilarPercentage("This is Kane",
@@ -37,11 +37,11 @@ public class MainStep {
 		// Test extracting translatable strings
 		ArrayList<ParsingRule> allRules = new ArrayList<ParsingRule>();
 		allRules.add(new ParsingRule("^(.*?)=(.*?)$", 1, 2));
-		// for (TUnit tempUnit : ExtractTranslatableStrings.doExtract(
-		// "Sample/BasicResourceFile.txt", "UTF-8", allRules)) {
-		// System.out.print("ID:" + tempUnit.ID + "\n");
-		// System.out.print("String:" + tempUnit.originalString + "\n");
-		// }
+		for (TUnit tempUnit : ExtractTranslatableStrings.doExtract(
+			"Sample/BasicResourceFile.txt", "UTF-8", allRules)) {
+			System.out.print("ID:" + tempUnit.ID + "\n");
+			System.out.print("String:" + tempUnit.originalString + "\n");
+		}
 		String resourceFile = "Sample/BasicResourceFile.txt";
 		String resourceFile2 = "Sample/BasicResourceFile2.txt";
 		String outputXFile = "Sample/BasicResourceFile.xliff";
@@ -54,7 +54,8 @@ public class MainStep {
 		tempFile.create(allUnits, outputXFile, "enu");
 		// Test encoding conversion
 		// ConvertEncoding.convert("Sample/UTF8Sample.txt","Sample/GBKOutput.txt","UTF-8","GBK");
-
+		System.out.println("wahaha!");
 	}
 
 }
+
