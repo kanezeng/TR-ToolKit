f08e694c5800f0860629fdc13acd8cbd79a3381e
Kane Zeng <wintera.zeng@gmail.com>
2013-03-13 16:20:18 +0800

    Add a new class excluderule
    Use for storing regular expressions to exclude
    untranslatable/unlocalizable strings.

---
+++ src/com/kanezeng/Translation/EngObjects/ExcludeRule.java
@@ -0,0 +1,17 @@
+/**
+ *  Copyright 2013 Zhijian Zeng, All Rights Reserved.
+ *  Email: me@kanezeng.com
+ *	This file is created at 2013-3-13
+ */
+package com.kanezeng.Translation.EngObjects;
+
+/**
+ * Exclude rule is a regular expression used to exclude untranslatable/unlocalizable strings.
+ */
+public class ExcludeRule {
+	public String rule = "";
+	
+	public ExcludeRule(String rule) {
+		this.indexOfID = indexOfID;
+	}
+}
--- src/com/kanezeng/Translation/MainStep.java
+++ src/com/kanezeng/Translation/MainStep.java
@@ -14,7 +14,7 @@ import com.kanezeng.Translation.FileProcessing.XLIFFFile;
 import com.kanezeng.Translation.StringProcessing.LevenshteinDistance;
 
 /**
- * This is use by Kane to test the APIs only. You can simply ignore it.
+ * This is used by Kane to test the APIs only. You can simply ignore it.
  */
 public class MainStep {
 
@@ -23,15 +23,12 @@ public class MainStep {
 	 */
 	public static void main(String[] args) {
 		// Test Native2ASCII.toASCII()
-		// System.out.println(Native2ASCII.toASCII("wŶ����־��haha!"));
+		// System.out.println(Native2ASCII.toASCII("我是中国人haha!"));
 //		 Test LevenshteinDistance
-		 System.out.println(LevenshteinDistance.getDistance("This is Kane!!",		 "Tha is Kane"));
-		System.out.println(LevenshteinDistance.getDifferentPercentage("This is Kane",
-		"Tha is Kane"));
-		System.out.println(LevenshteinDistance.getSimilarPercentage("This is Kane",
-		"Thass is Kane"));
-		System.out.println(LevenshteinDistance.getSimilarPercentage("This is Kane",
-		"Thass is Kane",1));
+		 System.out.println(LevenshteinDistance.getDistance("This is Kane!!", "Tha is Kane"));
+		System.out.println(LevenshteinDistance.getDifferentPercentage("This is Kane", "Tha is Kane"));
+		System.out.println(LevenshteinDistance.getSimilarPercentage("This is Kane", "Thass is Kane"));
+		// System.out.println(LevenshteinDistance.getSimilarPercentage("This is Kane",	"Thass is Kane",1));
 		// Test Word Count
 		// System.out.println(WordCount.getWordCount("     haha I am Kane!    "));
 		// Test extracting translatable strings
